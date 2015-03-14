2c8f16226dd14f04708d7557974e542df94c3290
Tom <tommy@daftscience.com>
2015-03-14 12:10:46 -0500

    Update database.py

--- database.py
+++ database.py
@@ -148,7 +148,7 @@ class tiny_db():
                 self.mem_db.append(item)
             print "found: rack  " + str(item['rack']) + " " + str(item['column']) + " " + str(item['row'])+ " " + str(item['time'])
         self.db.remove(eids=purge)
-        # self.last_filed = self.db.get(eid=max(purge))
+        self.last_filed = self.db.get(eid=[max(purge)])
         return self.mem_db
             
         
@@ -184,7 +184,7 @@ class tiny_db():
                 self.mem_db.append(item)
             print "found: rack  " + str(item['rack']) + " " + str(item['column']) + " " + str(item['row'])+ " " + str(item['time'])
         # self.db.remove(eids=purge)
-        # self.last_filed = self.db.get(eid=max(purge))
+        self.last_filed = self.db.get(eid=[max(purge)])
         return self.mem_db
         
 RACK_DB = tiny_db()
